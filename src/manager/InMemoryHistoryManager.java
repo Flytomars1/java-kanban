28fc226709d7664e663ad3e94cb1cc9ab9ef0260
flytomars <tramp.box@gmail.com>
2025-06-23 17:13:18 +0300

    Исправлена работа истории просмотров: удаление дубликатов и оптимизация производительности

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -4,23 +4,98 @@ import model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history = new ArrayList<>();
-    private final static int MAX_HISTORY_SIZE = 10;
+    private final Map<Integer, Node> historyMap = new HashMap<>();
+    private final HistoryLinkedList historyList = new HistoryLinkedList();
+
+
 
     //добавление задач в историю
     @Override
     public void add(Task task) {
-        history.add(task);
+        if (task == null) return;
+        int taskId = task.getId();
 
-        if (history.size() > MAX_HISTORY_SIZE) {
-            history.remove(0);
+        if (historyMap.containsKey(taskId)) {
+            remove(taskId);
         }
+
+        //будем хранить в истории не задачу, а копию задачи
+        Task copy = new Task(task.getTitle(), task.getDescription());
+        copy.setId(task.getId());
+        copy.setStatus(task.getStatus());
+        copy.setDescription(task.getDescription());
+        copy.setTitle(task.getTitle());
+
+        historyList.linkLast(copy);
+        historyMap.put(taskId, historyList.getLastNode());
+    }
+
+    @Override
+    public void remove(int id) {
+        Node nodeToRemove = historyMap.get(id);
+        if (nodeToRemove == null) return;
+
+        historyList.removeNode(nodeToRemove);
+        historyMap.remove(id);
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        List<Task> result = new ArrayList<>();
+        Node current = historyList.getHeadNode();
+        while (current != null) {
+            result.add(current.task);
+            current = current.next;
+        }
+
+        return result;
     }
+
+    private class HistoryLinkedList {
+        private Node head;
+        private Node tail;
+
+        public void linkLast(Task task) {
+            Node newNode = new Node(task);
+            if (tail == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.prev = tail;
+                tail.next = newNode;
+
+                tail = newNode;
+            }
+        }
+
+        public void removeNode(Node node) {
+            Node prevNode = node.prev;
+            Node nextNode = node.next;
+
+            if (prevNode != null) {
+                prevNode.next = nextNode;
+            } else {
+                head = nextNode;
+            }
+
+            if (nextNode != null) {
+                nextNode.prev = prevNode;
+            } else {
+                tail = prevNode;
+            }
+        }
+
+        public Node getHeadNode() {
+            return head;
+        }
+
+        public Node getLastNode() {
+            return tail;
+        }
+    }
+
 }
