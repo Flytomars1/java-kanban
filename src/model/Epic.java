61aed2ce82045fdd64676f40e9a49060cd6ab4e6
flytomars <tramp.box@gmail.com>
2025-07-22 03:15:24 +0300

    Добавлены новые поля в классы, добавлены методы для калькуляции полей startTima в эпик и endTime для всех классов, переделал toString

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -1,12 +1,16 @@
 package model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Epic extends Task {
     private final ArrayList<Integer> subtaskIds = new ArrayList<>();
+    private LocalDateTime endTime;
 
     public Epic(String title, String description) {
-        super(title, description);
+        super(title, description, null, null);
     }
 
     public void addSubtaskId(int id) {
@@ -28,13 +32,55 @@ public class Epic extends Task {
         return subtaskIds;
     }
 
+    public LocalDateTime calculateStartTime(List<Subtask> allSubtasks) {
+        return allSubtasks.stream()
+                .filter(st -> subtaskIds.contains(st.getId()))
+                .map(Task::getStartTime)
+                .min(LocalDateTime::compareTo)
+                .orElse(null);
+    }
+
+    public Duration calculateDuration(List<Subtask> allSubtasks) {
+        return allSubtasks.stream()
+                .filter(st -> subtaskIds.contains(st.getId()))
+                .map(Task::getDuration)
+                .reduce(Duration.ZERO, Duration::plus);
+    }
+
+    public LocalDateTime calculateEndTime(List<Subtask> allSubtasks) {
+        return allSubtasks.stream()
+                .filter(st -> subtaskIds.contains((st.getId())))
+                .map(Task::getEndTime)
+                .max(LocalDateTime::compareTo)
+                .orElse(null);
+    }
+
+    public void updateEpicFromSubtasks(List<Subtask> allSubtasks) {
+        LocalDateTime newStartTime = calculateStartTime(allSubtasks);
+        Duration newDuration = calculateDuration(allSubtasks);
+        LocalDateTime newEndTime = calculateEndTime(allSubtasks);
+
+        super.setStartTime(newStartTime);
+        super.setDuration(newDuration);
+        this.endTime = newEndTime;
+    }
+
+    @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+
     @Override
     public String toString() {
-        return "model.Epic{" +
+        return "Epic{" +
                 "title='" + getTitle() + '\'' +
                 ", description='" + getDescription() + '\'' +
                 ", id=" + getId() +
                 ", status=" + getStatus() +
+                ", startTime=" + getStartTime() +
+                ", duration=" + getDuration() +
+                ", endTime=" + getEndTime() +
                 '}';
     }
 }
--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,10 +1,13 @@
 package model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Subtask extends Task {
     private int epicId;
 
-    public Subtask(String title, String description, int epicId) {
-        super(title, description);
+    public Subtask(String title, String description, LocalDateTime startTime, Duration duration, int epicId) {
+        super(title, description, startTime, duration);
         this.epicId = epicId;
     }
 
@@ -23,6 +26,8 @@ public class Subtask extends Task {
                 ", description='" + getDescription() + '\'' +
                 ", id=" + getId() +
                 ", status=" + getStatus() +
+                ", startTime=" + getStartTime() +
+                ", duration=" + getDuration() +
                 ", epicId=" + epicId +
                 '}';
     }
